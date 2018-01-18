e304369155ac9b4125c0334be9990a6b8e192bce
Adaming44 <Adaming44@192.168.1.148>
2018-01-18 11:02:11 +0100

    LAra : classe commande et client

---
+++ Ecom/src/main/java/fr/adaming/model/Client.java
@@ -0,0 +1,108 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="clients")
+public class Client implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idClient; 
+	
+	private String nomClient;
+	
+	private String adresse;
+	
+	private String email;
+	
+	private String tel;
+	
+	//transformation uml en java
+	@OneToMany(mappedBy="client")
+	private List<Commande> listeCommande;
+
+	public Long getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public List<Commande> getListeCommande() {
+		return listeCommande;
+	}
+
+	public void setListeCommande(List<Commande> listeCommande) {
+		this.listeCommande = listeCommande;
+	}
+
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(String nomClient, String adresse, String email, String tel) {
+		super();
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	public Client(Long idClient, String nomClient, String adresse, String email, String tel) {
+		super();
+		this.idClient = idClient;
+		this.nomClient = nomClient;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [nomClient=" + nomClient + ", adresse=" + adresse + ", email=" + email + ", tel=" + tel + "]";
+	}
+	
+}
--- Ecom/src/main/java/fr/adaming/model/Commande.java
+++ Ecom/src/main/java/fr/adaming/model/Commande.java
@@ -1,12 +1,80 @@
 package fr.adaming.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity // pour que la classe test soit persistante
+@Table(name="commandes")
 public class Commande implements Serializable{
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idCommande;
 	
+	@Temporal(TemporalType.DATE)
+	private Date dateCommande;
+	
+	//transformation uml en java
+	@OneToMany(mappedBy="commande")
+	private List<LigneCommande> listeLigneCommande;
+	
+	@ManyToOne
+	@JoinColumn(referencedColumnName="idClient")
+	private Client client;
 
+	public Long getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(Long idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+	
+	public List<LigneCommande> getListeLigneCommande() {
+		return listeLigneCommande;
+	}
+
+	public void setListeLigneCommande(List<LigneCommande> listeLigneCommande) {
+		this.listeLigneCommande = listeLigneCommande;
+	}
+	
+	public Commande() {
+		super();
+	}
+
+	public Commande(Date dateCommande) {
+		super();
+		this.dateCommande = dateCommande;
+	}
+
+	public Commande(Long idCommande, Date dateCommande) {
+		super();
+		this.idCommande = idCommande;
+		this.dateCommande = dateCommande;
+	}
+
+	@Override
+	public String toString() {
+		return "Commande [idCommande=" + idCommande + ", dateCommande=" + dateCommande + "]";
+	}
 }
