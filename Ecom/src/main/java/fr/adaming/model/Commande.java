0c912cb2bcad56ca96fd5058852703887831ff0c
Adaming44 <Adaming44@192.168.1.148>
2018-01-18 10:53:11 +0100

    lara : entity entier

--- Ecom/src/main/java/fr/adaming/model/Commande.java
+++ Ecom/src/main/java/fr/adaming/model/Commande.java
@@ -2,7 +2,9 @@ package fr.adaming.model;
 
 import java.io.Serializable;
 
-@ent
+import javax.persistence.Entity;
+
+@Entity // pour que la classe test soit persistante
 public class Commande implements Serializable{
 	
 	
